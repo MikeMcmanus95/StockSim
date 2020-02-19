23fe73053b65f82280330c318029bff4013f1378
Michael Mcmanus <mikemcmanus95@gmail.com>
2020-02-19 17:20:58 -0500

    refactor: About page

--- client/components/About.js
+++ client/components/About.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 export const About = () => {
   return (
-    <div>
-      <h1>Welcome to Stock Sim!</h1>
-      <p>
-        This is a free stock buying simulator. Built as an entry for the NYC
-        Tech Talent Pipeline assessment.
-      </p>
-      <small>Built with ❤️ by Michael Mcmanus</small>
-    </div>
+    <>
+      <div className="landing">
+        <h1>Welcome to Stock Sim!</h1>
+        <p>
+          This is a free stock buying simulator. Built as an entry for the NYC
+          Tech Talent Pipeline assessment.
+        </p>
+      </div>
+      <div className="footer">
+        <small>Built with ❤️ by Michael Mcmanus</small>
+      </div>
+    </>
   );
 };
